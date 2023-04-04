6d1ee982e893ee7c89a09849a174076b3a922704
孤独的键 <aifound@88.com>
2023-04-04 16:31:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="8080-885be2330e3b49008a150a14ad9b7849.onpatr.cloud";
+        url.hostname="serverstatus.hillteam.repl.co";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
