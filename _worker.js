5f9fbaf4a844e57250fb6d961566b4ce906a510c
孤独的键 <aifound@88.com>
2023-04-20 16:24:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="serverstatus.hillteam.repl.co";
+        url.hostname="serverstatus.beer-on-ice.repl.co";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
